680f114a7e16a197cb45ee3b086196343a457a50
JordyCastillo33 <jordyjosuecastillo10@gmail.com>
2021-03-21 18:00:50 -0600

    Interfaz del color verde

--- PracticaGIT/Verde.xaml.cs
+++ PracticaGIT/Verde.xaml.cs
@@ -10,26 +10,25 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace PracticaGIT
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Verde.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Verde : Window
     {
-        public MainWindow()
+        public Verde()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            Verde verde = new Verde();
-            verde.Show();
-            this.Close();
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            
         }
     }
 }
