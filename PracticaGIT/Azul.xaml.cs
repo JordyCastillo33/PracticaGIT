e300b61b5fe9a2431904b765c7bec73af46121de
Osman Gomez <gomezosman05@gmail.com>
2021-03-21 18:15:36 -0600

    Confirmar

--- PracticaGIT/Azul.xaml.cs
+++ PracticaGIT/Azul.xaml.cs
@@ -10,26 +10,24 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace PracticaGIT
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Azul.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Azul : Window
     {
-        public MainWindow()
+        public Azul()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            Azul azul = new Azul();
-            azul.Show();
-            this.Close();
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
         }
     }
 }
